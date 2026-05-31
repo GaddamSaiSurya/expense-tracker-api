1c726d3d20d396a283bad302e6801efa3bc15fff
Surya <surya104441@gmail.com>
2026-05-31 21:02:22 +0530

    add entity

---
+++ src/main/java/com/example/expenseTracker/entity/Expense.java
@@ -0,0 +1,88 @@
+package com.example.expenseTracker.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class Expense {
+
+    @Id
+    @SequenceGenerator(
+            name="expense_sequence",
+            sequenceName = "expense_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "expense_sequence"
+    )
+
+    private Long id;
+    private String title;
+    private BigDecimal amount;
+    private String category;
+    private LocalDate date;
+
+    public Expense() {
+    }
+
+    public Expense(String title, BigDecimal amount, String category, LocalDate date) {
+        this.title = title;
+        this.amount = amount;
+        this.category = category;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Expense{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", amount=" + amount +
+                ", category='" + category + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/expenseTracker/repository/ExpenseRepository.java
@@ -0,0 +1,4 @@
+package com.example.expenseTracker.repository;
+
+public class ExpenseRepository {
+}
