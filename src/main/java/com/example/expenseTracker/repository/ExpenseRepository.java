0b5beb279fa5f8fe8e8fbd38f1e2f87e4a864dd7
Surya <surya104441@gmail.com>
2026-05-31 21:07:50 +0530

    add repository

--- src/main/java/com/example/expenseTracker/repository/ExpenseRepository.java
+++ src/main/java/com/example/expenseTracker/repository/ExpenseRepository.java
@@ -1,4 +1,9 @@
 package com.example.expenseTracker.repository;
 
-public class ExpenseRepository {
+import com.example.expenseTracker.entity.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 }
